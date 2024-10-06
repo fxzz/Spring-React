8095ffead32e3919f9e9ec05b967d4ce1bcdd7d6
fxzz <fxzzy@naver.com>
2024-10-06 23:41:43 +0900

    react-router-setting

--- react/mall/src/App.js
+++ react/mall/src/App.js
@@ -1,9 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import root from "./router/root";
+
 function App() {
-  return (
-    <div className="text-4xl font-extrabold m-2 p-2 bg-green-500 underline">
-      hello world
-    </div>
-  );
+  return <RouterProvider router={root} />;
 }
 
 export default App;
---
+++ react/mall/src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div className="text-3xl">
+      <div>AboutPage</div>
+    </div>
+  );
+}
+
+export default AboutPage;
