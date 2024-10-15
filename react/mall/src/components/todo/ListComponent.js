24ba2572b2ee8db0f823dc668987e3b00581a58c
fxzz <fxzzy@naver.com>
2024-10-15 21:57:53 +0900

    hooks

--- react/mall/src/components/todo/ListComponent.js
+++ react/mall/src/components/todo/ListComponent.js
@@ -25,7 +25,30 @@ const ListComponent = () => {
       setServerData(data);
     });
   }, [page, size]);
-  return <div>1</div>;
+  return (
+    <div className="border-2 border-blue-100 mt-10 mr-2 ml-2">
+      <div className="flex flex-wrap mx-auto justify-center p-6">
+        {serverData.dtoList.map((todo) => (
+          <div
+            key={todo.tno}
+            className="w-full min-w-[400px] p-2 m-2 rounded shadow-md"
+          >
+            <div className="flex ">
+              <div className="font-extrabold text-2xl p-2 w-1/12">
+                {todo.tno}
+              </div>
+              <div className="text-1xl m-1 p-2 w-8/12 font-extrabold">
+                {todo.title}
+              </div>
+              <div className="text-1xl m-1 p-2 w-2/10 font-medium">
+                {todo.dueDate}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ListComponent;
